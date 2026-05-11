a5b050dca96ede974e852cd81e15c6c38940a3cb
Muhammad Zain Mumtaz <zain123@gmail.com>
2026-05-11 15:29:51 -0700

    update backend

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ mongoose
 // CORS
 const allowedOrigins = [
 //   process.env.FRONTEND_URL as string,
-"demosekaispacehotelapp.vercel.app",
+"https://demosekaispacehotelapp.vercel.app",
 "https://demosekaispacehotelapp-zainmumtaz999s-projects.vercel.app",
 "https://demosekaispacehotelapp-git-main-zainmumtaz999s-projects.vercel.app",
 "https://demosekaispacehotelapp-fx3a46k83-zainmumtaz999s-projects.vercel.app",
